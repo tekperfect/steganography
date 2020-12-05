4a2794e3f5d94f2d577b647149e1ed33263083ea
NinjaAung <NinjaAung02@gmail.com>
2020-12-05 15:18:49 -0800

    init

---
+++ main.py
@@ -0,0 +1,75 @@
+# TODO: Run `pip3 install Pillow` before running the code.
+from PIL import Image
+
+
+def decode_image(path_to_png):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    # Open the image using PIL:
+    encoded_image = Image.open(path_to_png)
+
+    # Separate the red channel from the rest of the image:
+    red_channel = encoded_image.split()[0]
+
+    # Create a new PIL image with the same size as the encoded image:
+    decoded_image = Image.new("RGB", encoded_image.size)
+    pixels = decoded_image.load()
+    x_size, y_size = encoded_image.size
+
+    # TODO: Using the variables declared above, replace `print(red_channel)` with a complete implementation:
+    print(red_channel)  # Start coding here!
+
+    # DO NOT MODIFY. Save the decoded image to disk:
+    decoded_image.save("decoded_image.png")
+
+
+def encode_image(path_to_png):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    pass
+
+
+def write_text(text_to_write):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    pass# TODO: Run `pip3 install Pillow` before running the code.
+from PIL import Image
+
+
+def decode_image(path_to_png):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    # Open the image using PIL:
+    encoded_image = Image.open(path_to_png)
+
+    # Separate the red channel from the rest of the image:
+    red_channel = encoded_image.split()[0]
+
+    # Create a new PIL image with the same size as the encoded image:
+    decoded_image = Image.new("RGB", encoded_image.size)
+    pixels = decoded_image.load()
+    x_size, y_size = encoded_image.size
+
+    # TODO: Using the variables declared above, replace `print(red_channel)` with a complete implementation:
+    print(red_channel)  # Start coding here!
+
+    # DO NOT MODIFY. Save the decoded image to disk:
+    decoded_image.save("decoded_image.png")
+
+
+def encode_image(path_to_png):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    pass
+
+
+def write_text(text_to_write):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    pass
